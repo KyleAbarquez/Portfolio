9a62482852c802779f832320b2b54e855f2ec0ef
Kyle Abarquez <kabarquez8@gmail.com>
2018-07-19 11:23:55 -0700

    fixed header scroll animations

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
         if($(window).scrollTop() > 0){
             $("header").css({"height":"70px"});
             $("#logo").css({"font-size":"27px"});
+            $("#logo").css({"transform":"translateY(-7px)"});
+            $("nav ul").css({"transform":"translateY(-5px)"});
         } 
         else{
             $("header").css({"height":"90px"});
             $("#logo").css({"font-size":"32px"});
+            $("#logo").css({"transform":"translateY(0px)"});
+            $("nav ul").css({"transform":"translateY(0px)"});
         }
 
     })
