4c2749983a521a7516baac92874ba2012f6e6248
Kyle Abarquez <kabarquez8@gmail.com>
2018-07-19 11:44:41 -0700

    changed header styles

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 0){
-            $("header").css({"height":"70px"});
-            $("#logo").css({"font-size":"27px"});
-            $("#logo").css({"transform":"translateY(-7px)"});
-            $("nav ul").css({"transform":"translateY(-5px)"});
-        } 
-        else{
-            $("header").css({"height":"90px"});
-            $("#logo").css({"font-size":"32px"});
-            $("#logo").css({"transform":"translateY(0px)"});
-            $("nav ul").css({"transform":"translateY(0px)"});
-        }
-
-    })
-})
+//$(document).ready(function(){
+//    $(window).scroll(function(){
+//        if($(window).scrollTop() > 0){
+//            $("header").css({"height":"70px"});
+//            $("#logo").css({"font-size":"27px"});
+//            $("#logo").css({"transform":"translateY(-7px)"});
+//            $("nav ul").css({"transform":"translateY(-5px)"});
+//        } 
+//        else{
+//            $("header").css({"height":"90px"});
+//            $("#logo").css({"font-size":"27px"});
+//            $("#logo").css({"transform":"translateY(0px)"});
+//            $("nav ul").css({"transform":"translateY(0px)"});
+//        }
+//
+//    })
+//})
 
