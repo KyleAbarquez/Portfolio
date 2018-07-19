4b821d7ad2af1d748fbf303ce93285717c18d2fe
Kyle Abarquez <kabarquez8@gmail.com>
2018-07-19 10:31:28 -0700

    added link to top on logo

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop() > 0){
             $("header").css({"height":"70px"});
-            $(".logo").css({"margin-top":"20px"});
+            $("#logo").css({"margin-top":"-30px"});
         } 
         else{
             $("header").css({"height":"90px"});
-            $(".logo").css({"margin-top":"28px"});
+            $("#logo").css({"margin-top":"7px"});
         }
 
     })
