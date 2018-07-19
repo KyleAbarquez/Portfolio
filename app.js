ef9f5ba2c408590e468cecec48564d1b65faf510
Kyle Abarquez <kabarquez8@gmail.com>
2018-07-19 11:02:40 -0700

    fixed header margin issues

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if($(window).scrollTop() > 0){
             $("header").css({"height":"70px"});
-            $("#logo").css({"margin-top":"-30px"});
+            $("#logo").css({"font-size":"27px"});
         } 
         else{
             $("header").css({"height":"90px"});
-            $("#logo").css({"margin-top":"7px"});
+            $("#logo").css({"font-size":"32px"});
         }
 
     })
