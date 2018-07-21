4acd2b82f00f9f663662f5629712d5450681ab34
Kyle Abarquez <kabarquez8@gmail.com>
2018-07-21 13:02:58 -0700

    more tweaks

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-//$(document).ready(function(){
-//    $(window).scroll(function(){
-//        if($(window).scrollTop() > 0){
-//            $("header").css({"height":"70px"});
-//            $("#logo").css({"font-size":"27px"});
-//            $("#logo").css({"transform":"translateY(-7px)"});
-//            $("nav ul").css({"transform":"translateY(-5px)"});
-//        } 
-//        else{
-//            $("header").css({"height":"90px"});
-//            $("#logo").css({"font-size":"27px"});
-//            $("#logo").css({"transform":"translateY(0px)"});
-//            $("nav ul").css({"transform":"translateY(0px)"});
-//        }
-//
-//    })
-//})
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 0){
+            $("header").css({"background-color":"#42c2f4"});
+            $("#logo").css({"color":"black"});
+            $("nav a").css({"color":"black"});
+        } 
+        else {
+            $("header").css({"background-color":"black"});
+            $("#logo").css({"color":"#FFF"});
+            $("nav a").css({"color":"#FFF"});
+        }
+    })
+})
 
