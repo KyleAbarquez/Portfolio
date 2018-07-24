67533121d3b4e1e9b42c7df303acfdb18f173dfa
Kyle Abarquez <kabarquez8@gmail.com>
2018-07-24 01:32:53 -0700

    nav changes

--- app.js
+++ app.js
@@ -4,12 +4,38 @@ $(document).ready(function(){
             $("header").css({"background-color":"#42c2f4"});
             $("#logo").css({"color":"black"});
             $("nav a").css({"color":"black"});
-        } 
+            $("nav ul").css({"background-color":"#42c2f4"});
+        }
+        
         else {
             $("header").css({"background-color":"black"});
             $("#logo").css({"color":"#FFF"});
             $("nav a").css({"color":"#FFF"});
+            $("nav ul").css({"background-color":"black"});
         }
     })
 })
+
+//$(document).ready(function(){
+//    $(window).scroll(function(){
+//        if($(window).scrollTop() > $( window ).height() - 20){
+//           $("#one-star").addClass('active-star'); 
+//        } 
+//        else if($(window).scrollTop() > $(window ).height() ){
+//            $("#one-star").removeClass('active-star');
+//        }
+//    })
+//})
+
+//     $(document).ready(function() {
+// 			$('#sticky-navigation').scrollspy({
+// 				min: $('#nav').offset().top,
+// 				onEnter: function(element, position) {
+// 					$("#nav").addClass('fixed');
+// 				},
+// 				onLeave: function(element, position) {
+// 					$("#nav").removeClass('fixed');
+// 				}
+// 			});
+// 		});
 
